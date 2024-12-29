c659532f428b9ccf0f24526535fed06eefd36a87
Cesar <wasa4587@gmail.com>
2024-12-28 23:05:29 -0600

    pagina y privacy policy

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,69 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: { BENTO_USERNAME: string; BASE_URL: string }): Promise<Response> {
+	  const path = new URL(request.url).pathname;
+	  let url = 'https://bento.me' + path;
+  
+	  if (path.includes('v1')) {
+		url = 'https://api.bento.me' + path;
+	  }
+	  if (path == '/privacy-policy') {
+		url = 'https://d0a1adcc.privacy-policy-2ci.pages.dev/';
+	  }
+	  if (url === 'https://bento.me/') {
+		url = `https://bento.me/partyhardio`;
+	  }
+  
+	  const headers: HeadersInit = {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+	  };
+	  console.log({url, path})
+	  const response = await fetch(url, { headers });
+	  const contentType = response.headers.get('content-type');
+	  let results = await parseResponseByContentType(response, contentType);
+	  if (!(results instanceof ArrayBuffer)) {
+		results = results.replaceAll('https://api.bento.me', "https://partyhard.io");
+	  }
+	  headers['content-type'] = contentType || 'text/plain';
+	  return new Response(results, { headers });
 	},
-} satisfies ExportedHandler<Env>;
+  };
+  
+  async function parseResponseByContentType(response: Response, contentType: string | null): Promise<string | ArrayBuffer> {
+	if (!contentType) return await response.text();
+  
+	switch (true) {
+	  case contentType.includes('application/json'):
+		return JSON.stringify(await response.json());
+	  case contentType.includes('text/html'):
+		const transformedResponse = new HTMLRewriter()
+		  .on('body', {
+			element(element) {
+			  element.append(
+				`
+				  <style>
+					/* Custom CSS you can add to modify the styling of your page */
+				  </style>
+				`,
+				{ html: true }
+			  );
+			  element.append(
+				`
+				  <script>
+					/* Custom JS you can add to modify something on your page */
+				  </script>
+				`,
+				{ html: true }
+			  );
+			},
+		  })
+		  .transform(response);
+		return await transformedResponse.text();
+	  case contentType.includes('font'):
+	  case contentType.includes('image'):
+		return await response.arrayBuffer();
+	  default:
+		return await response.text();
+	}
+  }
+
